739fda287036ba61a2a14cea2e58a3e2aae53c15
Bromka <efimov.iliya@gmail.com>
2020-05-24 22:55:17 +0300

    webpack config end

--- src/bundle.js
+++ src/bundle.js
@@ -1,1 +1,2 @@
-console.log ('module export');
+console.log('module export');
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import bundle from './bundle';
-import css from './scss/main.scss'
-console.log("Hello");
-async function test() {
-    await Promise.resolve();
-}
-
-test();
+import './scss/main.scss'
